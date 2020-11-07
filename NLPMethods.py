7acf2812f2e97b0a3f35fc18632c640407599fc4
tcifelli <tcifelli@stevens.edu>
2020-11-06 19:37:22 -0500

    updated most common POS count

--- NLPMethods.py
+++ NLPMethods.py
@@ -74,9 +74,9 @@ def MostCommonPOS(self):
     print("Most common", n, "adjectives:", Counter(adjectives).most_common(n))
     print("Most common", n, "verbs:", Counter(verbs).most_common(n))
 
-    return {'mostCommonNoun': Counter(nouns).most_common(n),
-            'mostCommonAdjective': Counter(adjectives).most_common(n),
-            'mostCommonVerb': Counter(verbs).most_common(n)}
+    return {'mostCommonNoun': Counter(nouns).most_common(n)[0][0] if Counter(nouns).most_common(n)[0][0] is not [] else None,
+            'mostCommonAdjective': Counter(adjectives).most_common(n)[0][0] if Counter(nouns).most_common(n)[0][0] is not [] else None,
+            'mostCommonVerb': Counter(verbs).most_common(n)[0][0] if Counter(nouns).most_common(n)[0][0] is not [] else None}
 
 def countwords(inputs):
     # Counts the number of words and characters in the string
